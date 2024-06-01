c4d2466759899254293b6b27048cddcccfefc079
Joachim-barre <joachim.barre22@laposte.net>
2024-06-01 15:20:57 +0200

    updated swipe

--- scripts/SwipeToKey.cs
+++ scripts/SwipeToKey.cs
@@ -2,33 +2,31 @@ using Godot;
 
 public partial class SwipeToKey : Node2D
 {
-    public float SwipeMin = 0.5f;
+    public float SwipeMin = 0.3f;
     public Vector2? SwipeStart = null;
 
-    public override void _UnhandledInput(InputEvent @event){
-            if(@event is InputEventMouseButton eventMouseButton){
-                if(eventMouseButton.IsActionPressed("click")){
-                    SwipeStart = eventMouseButton.Position;
-                }else if(eventMouseButton.IsActionReleased("click")){
-                    if(SwipeStart == null){
-                        return;
-                    }
-                    Log.dbg($"Swipe detected from : {SwipeStart} to : {eventMouseButton.Position}");
-                    Vector2 Swipe = (Vector2)SwipeStart - eventMouseButton.Position;
-                    var SwipeMinScaled = GetViewportRect().Size * SwipeMin;
-                    if(Swipe.Abs().X >= SwipeMinScaled.X && !(Swipe.Abs().Y >= SwipeMinScaled.Y)){
-                        if(Swipe.X >= 0){
-                            Input.ActionPress("move_right");
-                        }else
-                            Input.ActionPress("move_left");
-                    }if(Swipe.Abs().Y >= SwipeMinScaled.Y){
-                        if(Swipe.Y >= 0){
-                            Input.ActionPress("move_down");
-                        }else
-                            Input.ActionPress("move_up");
-                    }
-                    SwipeStart = null;
+    public override void _Process(double delta){
+            if(Input.IsActionJustPressed("swipe")){
+                SwipeStart = GetViewport().GetMousePosition();
+            }else if(Input.IsActionJustReleased("swipe")){
+                if(SwipeStart == null){
+                    return;
                 }
+                Log.dbg($"Swipe detected from : {SwipeStart} to : {GetViewport().GetMousePosition()}");
+                Vector2 Swipe = (Vector2)SwipeStart - GetViewport().GetMousePosition();
+                var SwipeMinScaled = GetViewportRect().Size * SwipeMin;
+                if(Swipe.Abs().X >= SwipeMinScaled.X && !(Swipe.Abs().Y >= SwipeMinScaled.Y)){
+                    if(Swipe.X <= 0){
+                        Input.ActionPress("move_right");
+                    }else
+                        Input.ActionPress("move_left");
+                }if(Swipe.Abs().Y >= SwipeMinScaled.Y){
+                    if(Swipe.Y <= 0){
+                        Input.ActionPress("move_down");
+                    }else
+                        Input.ActionPress("move_up");
+                }
+                SwipeStart = null;
             }
     }
 }

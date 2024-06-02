57f625bd74a908050cfe4a53909e1dfb12f833c3
Joachim-barre <joachim.barre22@laposte.net>
2024-06-02 15:43:22 +0200

    added save data methods

--- scripts/SaveData.cs
+++ scripts/SaveData.cs
@@ -8,4 +8,29 @@ public partial class SaveData : Resource
 
     [Export]
     public int Score;
+
+    public int[,] GetVals(){
+        if(vals.Count != 16){
+            return null;
+        }
+        var Res = new  int[,]{
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0},
+        };
+        for(int i=0;i!=4;i++)
+            for(int j=0;j!=4;j++)
+                Res[i,j]=vals[i+j*4];
+        return Res;
+    }
+
+    public void LoadVals(int[,] _vals){
+        if(_vals.GetLength(0)!=4||_vals.GetLength(1)!=4){
+            return;
+        }
+        for(int i=0;i!=4;i++)
+            for(int j=0;j!=4;j++)
+                vals[i+j*4]=_vals[i,j];
+    }
 }

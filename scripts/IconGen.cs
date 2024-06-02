db1d02284ced7f2f4570074d1c321cd4c4144e21
Joachim-barre <joachim.barre22@laposte.net>
2024-06-02 13:01:45 +0200

    added image saving code

--- scripts/IconGen.cs
+++ scripts/IconGen.cs
@@ -1,6 +1,17 @@
 using Godot;
-using System;
 
 public partial class IconGen : Node
 {
+    public override void _Ready(){
+        GetNode<tile>("MainIcon/Tile").SetValue(2048);
+        RenderingServer.FramePostDraw += SaveViewports;
+    }
+
+    public void SaveViewports(){
+        Log.info($"saving main icon to : {ProjectSettings.GlobalizePath("user://main_icon.png")}");
+        GetNode<SubViewport>("MainIcon").GetTexture().GetImage().SavePng("user://main_icon.png");
+        RenderingServer.FramePostDraw -= SaveViewports;
+        Log.info("Image saved quitting");
+        GetTree().Quit();
+    }
 }

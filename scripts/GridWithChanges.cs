6e89689f21cf671eb21fdfa6de595718dbe76ee6
Joachim-barre <joachim.barre22@laposte.net>
2024-06-29 14:33:03 +0200

    created Monad-ish GridWithChange class

---
+++ scripts/GridWithChanges.cs
@@ -0,0 +1,23 @@
+using Godot;
+using Godot.Collections;
+using GridChange;
+using System.Linq;
+
+public partial class GridWithChanges : GodotObject{
+    public Grid grid;
+    public Array<GridChange> changes;
+
+    public GridWithChanges(Grid _grid){
+        grid = _grid;
+    }
+
+    public GridWithChanges Bind(System.Func<Grid, GridWithChanges> func){
+        var res = func(grid);
+        res.changes = changes.Cast<GridChange>.Concat(res.changes.Cast<GridChange>).ToArray();
+        return res;
+    }
+
+    public Grid Unwrap(){
+        return grid;
+    }
+}

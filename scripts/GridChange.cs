e035a6c721eb3efd63dcceec57d957da12680e48
Joachim-barre <joachim.barre22@laposte.net>
2024-07-01 17:40:29 +0200

    added bush grid change

--- scripts/GridChange.cs
+++ scripts/GridChange.cs
@@ -4,4 +4,13 @@ namespace GridChanges {
     public abstract partial class GridChange : GodotObject{
         public abstract void Apply(Tile[,] _tiles);
     }
+
+    public abstract partial class Push : GridChange{
+        public Vector2 Tile1;
+        public Vector2 Tile2;
+
+        public override void Apply(Tile[,] _tiles){
+
+        }
+    }
 }
